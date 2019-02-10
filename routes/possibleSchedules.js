3382ed4d57b5f29eebf3f7a5a6275652ba752ce2
Tamuz <Tamuzhod@gmail.com>
2019-02-10 13:59:31 -0800

    Test

--- routes/possibleSchedules.js
+++ routes/possibleSchedules.js
@@ -1,3 +1,9 @@
+var data = require('../classes.json');
+
 exports.view = function(req, res){
-    res.render('possibleSchedules');
+    res.render('possibleSchedules', data);
+    dp.events.list = data;
+    dp.update();
   };
+
+
