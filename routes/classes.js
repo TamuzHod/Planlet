8d3d2e5496227457a114f63dddbac49a4f712a0d
Neve Foresti <nforesti@ucsd.edu>
2019-02-12 12:06:28 -0800

    fixed json integration for classes page

--- routes/classes.js
+++ routes/classes.js
@@ -3,5 +3,5 @@ var data = require('../classes.json');
 exports.view = function(req, res){
 	var major = req.params.major; 
 	console.log("The major  is: " + major);
- 	res.render('classes', data);
+ 	res.render('classes', [data, {'majorName': major}]);
 };
