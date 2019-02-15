92fb674905f878c5421df47a00e0b538aff40f68
Neve Foresti <nforesti@ucsd.edu>
2019-02-14 22:05:59 -0800

    links

--- routes/possibleSchedules.js
+++ routes/possibleSchedules.js
@@ -1,6 +1,12 @@
- 
- exports.view = function(req, res){
-    res.render('possibleSchedules');
- };
 
+/*
+ * GET availability reset page.
+ */
 
+exports.view = function(req, res){
+	var major = req.params.major; 
+	var minor = req.params.minor; 
+	var college = req.params.college; 
+
+ 	res.render('possibleSchedules', {'majorName': major, 'minorName': minor, 'collegeName': college});
+};
