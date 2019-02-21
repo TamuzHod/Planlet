f263ec98cdc3bfddf9f5e39bd8189539cba5e992
Tamuz <Tamuzhod@gmail.com>
2019-02-21 12:16:54 -0800

    Fixed local

---
+++ routes/sendSelectedClasses.js
@@ -0,0 +1,21 @@
+
+//var selectedClasses = require('./selectedClasses.json');
+
+var fs = require("fs");
+var path = require('path');
+
+var content;
+exports.send = function(req, res){
+    var filePath = path.dirname(require.main.filename) + "/selectedClasses.json";
+	fs.readFile(filePath, "utf8", function read(err, data) {
+	    if (err) {
+	    	res.json("Failed to read file")
+	        throw err;
+	    }
+	    content = data;
+
+	    // Invoke the next step here however you like
+	    console.log(content);   // Put all of the code here (not the best solution)
+	    res.json(JSON.parse(content));          // Or put the next step in a function and invoke it
+	});
+};
