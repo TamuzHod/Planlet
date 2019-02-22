5f502332f45c44dcb9b6d4d596a6537ec4084e57
Tamuz <Tamuzhod@gmail.com>
2019-02-21 22:30:40 -0800

    ocontent = req.app.locals.slectedClassesJson; 	res.json(content);

--- routes/selectedClasses.js
+++ routes/selectedClasses.js
@@ -3,19 +3,19 @@ var path = require('path');
 
 
 exports.view = function(req, res){
-    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
-	fs.writeFile(filePath, JSON.stringify(req.body, null, 4), (err) => {
-    if (err) {
-        console.error(err);
-        res.send(filePath + " error");
+ //    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
+	// fs.writeFile(filePath, JSON.stringify(req.body, null, 4), (err) => {
+ //    if (err) {
+ //        console.error(err);
+ //        res.send(filePath + " error");
 
-        return;
-    };
-    	console.log("File has been created: " + filePath);
-    	console.log(JSON.stringify(req.body));
+ //        return;
+ //    };
+ //    	//console.log("File has been created: " + filePath);
+ //    	//console.log(JSON.stringify(req.body));
 
-	});
+	// });
     req.app.locals.slectedClassesJson = JSON.stringify(req.body, null, 4);
-
+    console.log(req.app.locals.slectedClassesJson);
     res.json(JSON.stringify(req.body, null, 4))
 };
--- routes/sendSelectedClasses.js
+++ routes/sendSelectedClasses.js
@@ -6,16 +6,14 @@ var path = require('path');
 
 var content;
 exports.send = function(req, res){
-    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
-	fs.readFile(filePath, "utf8", function read(err, data) {
-	    if (err) {
-	    	res.json("Failed to read file")
-	        throw err;
-	    }
-	    content = req.app.locals.slectedClassesJson;
-
-	    // Invoke the next step here however you like
-	    console.log(content);   // Put all of the code here (not the best solution)
-	    res.json(JSON.parse(content));          // Or put the next step in a function and invoke it
-	});
+ //    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
+	// fs.readFile(filePath, "utf8", function read(err, data) {
+	//     if (err) {
+	//     	res.json("Failed to read file")
+	//         throw err;
+	//     }
+	// });
+	content = req.app.locals.slectedClassesJson;
+	res.json(JSON.parse(content));          // Or put the next step in a function and invoke it
+	
 };
