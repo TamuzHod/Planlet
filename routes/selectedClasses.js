9ad7301f4f1d1159262e70ea69d4d516c2069137
TamuzHod <tamuzhod@gmail.com>
2019-03-05 00:30:04 -0800

    Added database

--- routes/selectedClasses.js
+++ routes/selectedClasses.js
@@ -1,21 +1,12 @@
 var fs = require("fs");
 var path = require('path');
-
+const crypto = require('crypto');
 
 exports.view = function(req, res){
- //    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
-	// fs.writeFile(filePath, JSON.stringify(req.body, null, 4), (err) => {
- //    if (err) {
- //        console.error(err);
- //        res.send(filePath + " error");
+   // req.app.locals.slectedClassesJson = JSON.stringify(req.body, null, 4);
+   // console.log(req.app.locals.slectedClassesJson);
+   // res.json(JSON.stringify(req.body, null, 4))
 
- //        return;
- //    };
- //    	//console.log("File has been created: " + filePath);
- //    	//console.log(JSON.stringify(req.body));
+  
 
-	// });
-    req.app.locals.slectedClassesJson = JSON.stringify(req.body, null, 4);
-    console.log(req.app.locals.slectedClassesJson);
-    res.json(JSON.stringify(req.body, null, 4))
 };
