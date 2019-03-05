a0a0d9d52f3fe9913275a17a8d99591a669d511c
Tamuz <Tamuzhod@gmail.com>
2019-03-05 00:46:03 -0800

    Testing database

--- routes/selectedClasses.js
+++ routes/selectedClasses.js
@@ -1,12 +1,48 @@
 var fs = require("fs");
 var path = require('path');
-const crypto = require('crypto');
-
+var dataStore;
 exports.view = function(req, res){
    // req.app.locals.slectedClassesJson = JSON.stringify(req.body, null, 4);
    // console.log(req.app.locals.slectedClassesJson);
    // res.json(JSON.stringify(req.body, null, 4))
 
-  
+   // Create a visit record to be stored in the database
+   const selectedClasses = {
+   	timestamp: new Date(),
+   	jsonData: req.body
+   };
+
+   try {
+   	insertData(selectedClasses);
+   	res.json(selectedClasses.jsonData)
+   } catch (error) {
+   	res.send(error);
+   }
+
+/**
+ * Insert a selectedClasses record into the database.
+ *
+ * @param {object} selectedClasses The selectedClasses record to insert.
+ */
+   function insertData(selectedClasses) {
+ 	return req.app.locals.datastore.save({
+ 		key: datastore.key('selectedClasses'),
+ 		data: selectedClasses,
+ 	});
+ }
 
 };
+
+
+ /**
+ * Retrieve the latest 10 visit records from the database.
+ */
+ function getVisits() {
+ 	const query = datastore
+ 	.createQuery('visit')
+ 	.order('timestamp', {descending: true})
+ 	.limit(10);
+
+ 	return datastore.runQuery(query);
+ }
+
--- routes/sendSelectedClasses.js
+++ routes/sendSelectedClasses.js
@@ -6,14 +6,17 @@ var path = require('path');
 
 var content;
 exports.send = function(req, res){
- //    var filePath = path.dirname(require.main.filename) + "/tmp/selectedClasses.json";
-	// fs.readFile(filePath, "utf8", function read(err, data) {
-	//     if (err) {
-	//     	res.json("Failed to read file")
-	//         throw err;
-	//     }
-	// });
-	content = req.app.locals.slectedClassesJson;
-	res.json(JSON.parse(content));          // Or put the next step in a function and invoke it
+
+	//content = req.app.locals.slectedClassesJson;
+
+	const query = datastore
+	 	.createQuery('selectedClasses')
+	 	.order('timestamp', {descending: true})
+	 	.limit(1);
+
+ 	content = datastore.runQuery(query);
+
+	res.json(JSON.parse(content.jsonData));          
 	
 };
+
