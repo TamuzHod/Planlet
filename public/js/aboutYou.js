c9f3f510f3295e4e61c51400c5b91c1caa993a2e
Neve Foresti <nforesti@ucsd.edu>
2019-03-07 20:34:31 -0800

    bug in aboutYou.js

--- public/js/aboutYou.js
+++ public/js/aboutYou.js
@@ -9,7 +9,7 @@ function startNewSchedule() {
 	var major = $("#selMajor :selected").val();
 	var college =  $("#selCollege :selected").text();
 	var minor =  $("#selMinor :selected").val();
-	var email = $("#emailinput").text();
+	var email = $("#emailInput").text();
 
 	if(major == 'Select a Major'){
 		alert("Please select a major");
