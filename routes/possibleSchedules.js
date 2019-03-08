6fb70f76d53c755a9aa1ca0bb45e9f7f5d131e44
Tamuz <Tamuzhod@gmail.com>
2019-03-07 21:06:45 -0800

    Staff

--- routes/possibleSchedules.js
+++ routes/possibleSchedules.js
@@ -4,17 +4,12 @@
  */
 
 exports.view = function(req, res){
-	var email = req.params.email;
-	var major = req.params.major; 
-	var minor = req.params.minor; 
 	var college = req.params.college; 
 
  	res.render('possibleSchedulesA', email);
 };
 
 exports.viewAlt = function(req, res){
-	var major = req.params.major; 
-	var minor = req.params.minor; 
 	var college = req.params.college; 
 
  	res.render('possibleSchedulesB');
