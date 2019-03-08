b2d9c8ea25b3e9a96ea067ff29063ca5dd328dd6
Tamuz <Tamuzhod@gmail.com>
2019-03-07 18:13:18 -0800

    Added file

---
+++ public/js/dataBaseInteractions.js
@@ -0,0 +1,31 @@
+
+
+
+function update(kind, id, data){
+
+}
+
+function create(kind, id, data){
+
+}
+
+
+
+function logInOrRegister(address){
+	var prefix = (address=="logIn") ? '#' : '#new_';
+	var data{
+		email: $(prefix+"email"),
+    	password: $(prefix+"password")
+	}
+        $.postJSON('/'+address, data, function (result) {
+            if(!result.succses) {
+            	alert(result.error);
+            	return;
+            }
+       		window.location.href = results.address;
+        });
+}
+
+
+
+
