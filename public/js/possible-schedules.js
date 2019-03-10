3d656ed98528323e90fa4e523df4aa60d10ce344
Neve Foresti <nforesti@ucsd.edu>
2019-03-09 23:11:58 -0800

    parenthesis

--- public/js/possible-schedules.js
+++ public/js/possible-schedules.js
@@ -212,11 +212,6 @@ function seeNewSchedule(scheduleHTML) {
         console.log(popup.innerHTML);
     }
 
-
-        // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
-        $.fn.popup.defaults.pagecontainer = '#page'
-    }
-
     function createHTML() {
         var html = "";
         var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
